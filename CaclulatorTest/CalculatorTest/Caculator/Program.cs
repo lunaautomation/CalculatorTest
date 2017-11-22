591d84709ffdeb5ae534a796e7b987621c9bab42
Daniel Hitchcock <lunaautomation@outlook.com>
2017-11-22 12:41:42 +0000

    First Commit

---
+++ CaclulatorTest/CalculatorTest/Caculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caculator
+{
+    public class Calculator
+    {
+        public int Calc(string inward)
+        {
+            int returned = 0;
+            if (inward == null)
+            {
+                return returned;
+            }
+            var array = inward.Split(',');
+            
+
+            foreach (var number in array)
+            {
+                int add = int.Parse(number);
+                returned = returned + add;
+            }
+            if (returned == 0)
+            {
+                return returned;
+            }
+            return returned;
+        }
+    }
+}
---
+++ CaclulatorTest/CalculatorTest/CalculatorTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Caculator;
+using NUnit.Framework;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void CanReciveOneNumber()
+        {
+            var Calculate = new Calculator();
+            string testdata = "11";
+            int expected = 11;
+            int result = Calculate.Calc(testdata);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void CanAddTwoValidNumbers()
+        {
+            var Calculate = new Calculator();
+            string testdata = "11,11";
+            int expected = 22;
+            int result = Calculate.Calc(testdata);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void CanAddThreeValidNumbers()
+        {
+            var Calculate = new Calculator();
+            string testdata = "11,11,11";
+            int expected = 33;
+            int result = Calculate.Calc(testdata);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void CanHandleNullReturnsZero()
+        {
+            var Calculate = new Calculator();
+            int expected=0;
+            int result = Calculate.Calc(null);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void CanHandleSpecialCharacters()
+        {
+            var Calculate = new Calculator();
+            string testdata = "4,*,$";
+            int expected = 0;
+            int result = Calculate.Calc(testdata);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
